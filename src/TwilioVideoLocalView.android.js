14d8fdd610826e5446f6649237eea6c702d80500
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2021-03-14 08:58:29 -0700

    remove duplicate import

--- src/TwilioVideoLocalView.android.js
+++ src/TwilioVideoLocalView.android.js
@@ -8,7 +8,6 @@
 import { requireNativeComponent, View } from 'react-native'
 import PropTypes from 'prop-types'
 import React from 'react'
-import PropTypes from 'prop-types'
 
 const propTypes = {
   ...View.propTypes,
